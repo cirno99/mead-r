3f845284d7b7df65cf4bebaf8f90c5f42bd3b61d
cirno99 <1563800293@qq.com>
2022-11-20 21:46:29 +0800

    add convert into sound

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 [![mead-r](https://snapcraft.io/mead-r/badge.svg)](https://snapcraft.io/mead-r)
 
+This is a fork from **mead-r**, but also copied some code from **morse2sound**
+
 An application of morse-encode-and-decode rewriting in Rust programming language.
+
+# Thanks
+- [mead-r](https://snapcraft.io/mead-r)
+- [morse2sound](https://github.com/irevenko/morse2sound)
--- src/main.rs
+++ src/main.rs
@@ -24,21 +24,39 @@ SOFTWARE.
 
 use clap::{App, Arg};
 use morse;
+use std::{thread::sleep, time::Duration};
+use anyhow::Result;
+use rodio::Source;
+
+pub mod args;
+use crate::args::{Message, Unit};
+
 fn main() {
     let matches = App::new("mead-r")
         .version("0.1.0")
         .author("Jintao Yang <yjt950840@outlook.com>")
         .about("An application of morse-encode-and-decode rewrite in Rust programming language.")
-        .arg(Arg::with_name("encode string")
+        .arg(
+            Arg::with_name("encode string")
                 .short('e')
                 .long("encode")
                 .takes_value(true)
-                .help("String to encode."))
-        .arg(Arg::with_name("decode string")
+                .help("String to encode."),
+        )
+        .arg(
+            Arg::with_name("decode string")
                 .short('d')
                 .long("decode")
                 .takes_value(true)
-                .help("String to decode."))
+                .help("String to decode."),
+        )
+        .arg(
+            Arg::with_name("covert into sound")
+                .short('c')
+                .long("convert")
+                .takes_value(true)
+                .help("Covert into sound."),
+        )
         .get_matches();
     if let Some(s) = matches.value_of("encode string") {
         use_encode(s);
@@ -46,24 +64,68 @@ fn main() {
     if let Some(s) = matches.value_of("decode string") {
         use_decode(s);
     }
+    if let Some(s) = matches.value_of("covert into sound") {
+        let s = use_encode(s).expect("error");
+        morse2sound(s);
+    }
 }
 
-fn use_encode(str: &str){
-    match morse::encode::encode(str) {
-        Ok(x) => println!("{}", x),
+fn use_encode(s: &str) -> Option<String> {
+    match morse::encode::encode(s) {
+        Ok(x) => {
+            println!("{}", x);
+            Some(x)
+    },
         Err(e) => {
-            println!("The following chars were unsupported {:?}",
-                     e.unsupported_characters)
+            println!(
+                "The following chars were unsupported {:?}",
+                e.unsupported_characters
+            );
+            None
         }
     }
 }
 
-fn use_decode(str: &str) {
-    match morse::decode::decode(str) {
+fn use_decode(s: &str) {
+    match morse::decode::decode(s) {
         Ok(x) => println!("{}", x),
         Err(e) => {
-            println!("The following chars were unsupported {:?}",
-                     e.unsupported_characters)
+            println!(
+                "The following chars were unsupported {:?}",
+                e.unsupported_characters
+            )
         }
     }
+}
+
+fn morse2sound(message: String) -> Result<()> {
+    let (_stream, stream_handle) = rodio::OutputStream::try_default()?;
+    let sink = rodio::Sink::try_new(&stream_handle)?;
+
+    let tone = rodio::source::SineWave::new(900);
+    let dot_duration = Duration::from_millis(80);
+    let dot = tone.clone().take_duration(dot_duration);
+    let dash = tone.take_duration(dot_duration * 3);
+    let message = message.parse::<Message>()?;
+    for unit in message.0 {
+        match unit {
+            Unit::Dot => {
+                sink.append(dot.clone());
+                sink.sleep_until_end();
+                sleep(dot_duration);
+            }
+            Unit::Dash => {
+                sink.append(dash.clone());
+                sink.sleep_until_end();
+                sleep(dot_duration)
+            }
+            Unit::Space => {
+                sleep(dot_duration * 3);
+            }
+            Unit::Slash => {
+                sleep(dot_duration * 3);
+            }
+        }
+    }
+    Ok(())
 }
